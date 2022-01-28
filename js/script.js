66de083be6d836cd2c4146df85b876f8e61f2c91
AdrianaSantorio <adriana.santorio@gmail.com>
2022-01-28 16:00:09 +0100

    added done class

--- js/script.js
+++ js/script.js
@@ -18,11 +18,9 @@ Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 
-- creare HTML
--creare lista iniziale (array di oggetti)
--far si che la lista informi la lunghezza della lista e il testo dei li
-
-
+creare una funzione che se done è true, visualizzare il testo del todo sbarrato.
+creare una classe :done
+attribuire la classe done solo se done è true
 */
 
 
@@ -34,6 +32,9 @@ const root = new Vue ({
             {text: 'scrivere una bozza di html', done: false,},
             {text: 'renderizzare la lista nel dom', done: false,},
         ],
+    }, 
+    methods: {
+
     }
     
 
